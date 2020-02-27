dea4f67b6ecd9c79ccfa74a0f4e1d4765e643739
Sam Ahmed <2102926@talnet.nl>
2020-02-27 10:26:39 +0100

    Uitwerking van de exercise

--- tijd.php
+++ tijd.php
@@ -1,15 +1,17 @@
 <?php
 $S = "s";
+
 echo "Voer een bepaald aantal in met 's' er achter";
+
 $leeslijn = readline();
+
 $pos = strpos($leeslijn, $S);
+
 if ((bool)$pos === true){
   $leeslijn = trim($leeslijn, "s");
   echo $leeslijn . " seconden";
-} else {
+ } else {
   echo "doe wat ik vraag, fatoeman";
   exit;
-}
-
-
+ }
  ?>
